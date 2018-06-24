7493d2674e4378fcb4f20ea66184223e532542e3
k-harada413 <kazuhiro.harada@uhuru.jp>
2018-06-24 19:15:10 +0900

    init comit

---
+++ TESTS/es80a.test.js
@@ -0,0 +1,37 @@
+var BME280Node = require('../es80a.js');
+
+
+var forCreateNode = function (a, config) {
+    a.on = function (a, func) {
+        func(msg);
+    };
+    a.send = function () { return true; };
+};
+var registerType = function (type, func) {
+    nodelist[type] = func;
+};
+
+var es80a_init = function () {
+    this.nodes = {
+        createNode: forCreateNode,
+        registerType: registerType
+    };
+};
+
+describe('check properties', () => {
+    test('is scl(PTE24) and sda(PTE25)', () => {
+        var BME280 = new BME280_init();
+        new BME280Node(BME280);
+        var conf = {
+            scl: 'PTE24',
+            sda: 'PTE25'
+        };
+        msg = {
+            payload: 'default'
+        };
+        nodelist.BME280(conf);
+        expect(msg.payload.temperature).toBe(20);
+        expect(msg.payload.humidity).toBe(30);
+        expect(msg.payload.atmPressure).toBe(1013);
+    });
+});
---
+++ es80a.js
@@ -0,0 +1,42 @@
+module.exports = function(RED) {
+    'use strict';
+    function es80aNode(n) {
+        RED.nodes.createNode(this,n);
+        this.gpioPin = Number(n.gpioPin);
+        this.angle = (n.angle - 600) / 10;
+        var node = this;
+
+        node.on('input', function (msg) {
+            var Gpio = require('pigpio').Gpio,
+            motor = new Gpio(node.gpioPin, {mode: Gpio.OUTPUT}),
+            pulseWidth = node.angle;
+
+            var move_servo = function(){
+                motor.servoWrite(angle);
+            };
+
+            move_servo();
+
+            function start_servo(){
+               setInterval(function(){
+                    move.servo;
+                },1000);
+            }
+
+            var servo_stop = function(){
+                clearInterval(move_servo);
+            }
+
+            start_servo();
+
+            setTimeout(servo_stop,2000);
+            node.send(msg);
+        });
+    
+        node.on('close',function(){
+            nfc.pause();
+        });
+    }
+    RED.nodes.registerType("ES80A",es80aNode);
+
+}
