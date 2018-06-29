19aa01229edd7d131fadb79b9a633945654c5683
k-harada413 <kazuhiro.harada@uhuru.jp>
2018-06-29 09:52:09 +0900

    止まってる箇所探し

--- es80a.js
+++ es80a.js
@@ -4,39 +4,54 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         this.gpioPin = Number(n.gpioPin);
         this.angle = (Number(n.angle) * 10 ) + 600;
+        
+        console.log("gpioPin is " + this.gpioPin + " PWM is " +this.angle );
         var node = this;
 
+    
         var Gpio = require('pigpio').Gpio,
         motor = new Gpio(node.gpioPin, {mode: Gpio.OUTPUT}),
         pulseWidth = node.angle;
 
+        console.log("init");
+
         node.on('input', function (msg) {
-            var move_servo = function(){
-                motor.servoWrite(pulseWidth);
-            };
+            console.log("node.on");
+            
+            var start = start_supply(mortor,node.angle); // interval
+            
+            move_servo(motor,node.angle); //即時実行
+            console.log("move_servo");
 
-            move_servo();
+            start;
 
-            function start_servo(){
-               setInterval(function(){
-                    move_servo();
-                },1000);
-            }
+            setTimeout(stop_supply(start),start,2000);
 
-            var servo_stop = function(){
-                clearInterval(move_servo);
-            }
-
-            start_servo();
-
-            setTimeout(servo_stop,2000);
             node.send(msg);
         });
     
         node.on('close',function(){
-            //servo_stop;
+            stop_supply(start);
         });
     }
     RED.nodes.registerType("ES80A",es80aNode);
 
+}
+
+function start_supply(_motor,_angle){
+    console.log("interval");
+    setInterval(function(){
+        move(_motor,_angle);
+    },1000);
+    return true;
+}
+
+function stop_supply(func){
+    clearInterval(func);
+    return true;
+}
+
+function move(_motor,_angle){
+    motor.servoWrite(_angle);
+    return true;
 }
